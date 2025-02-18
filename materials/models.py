272961406e66c5a8ae2fd5af65d1577da6b86cb9
Polyaa <polinasatrajkina@gmail.com>
2025-02-18 13:49:42 +0300

    create two new models and app "materials"

--- materials/models.py
+++ materials/models.py
@@ -1,8 +1,29 @@
 from django.db import models
 
-# Create your models here.
-class Course:
-    pass
 
-class Lesson:
-    pass
+# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    preview_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural ='курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    video_link = models.URLField(max_length=200, blank=True, null=True, verbose_name='ссылка на видео')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    country = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
     USERNAME_FIELD = 'email'
