f6e1ee66d4c8c53cb8aa85cf73a4bc95791cb6ad
Polyaa <polinasatrajkina@gmail.com>
2025-02-25 16:19:00 +0300

    replace Payments model to users app

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,16 +3,22 @@ from rest_framework import serializers
 from materials.models import Course, Lesson, Payments
 
 
-class CourseSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializers(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializers(serializers.ModelSerializer):
+    lesson_quantity = serializers.SerializerMethodField()  # Количество уроков
+    lessons = LessonSerializers(many=True, read_only=True, source="lesson_set")  # Информация о всех уроках
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def get_lesson_quantity(self, obj):
+        return obj.lesson_set.count()  # Подсчет количества уроков
 
 
 class PaymentsSerializers(serializers.ModelSerializer):
