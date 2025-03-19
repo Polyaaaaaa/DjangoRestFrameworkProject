6a76f91b5d919c1c2dffa81a5ad632eda63a6841
Polyaa <polinasatrajkina@gmail.com>
2025-03-19 15:31:38 +0300

    rewrite urls

--- materials/urls.py
+++ materials/urls.py
@@ -28,34 +28,14 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 
 app_name = MaterialsConfig.name
 
 router = DefaultRouter()
 router.register(r"courses", CourseViewSet, basename="courses")
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 
 urlpatterns = [
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("courses/", CourseListAPIView.as_view(), name="courses"),
     path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
     path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
